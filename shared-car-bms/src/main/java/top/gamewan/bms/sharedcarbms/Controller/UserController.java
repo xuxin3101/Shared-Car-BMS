a5b5e56444f32e012440786dfe5c2e680944a657
xuxin3101 <xuxin3101@gmail.com>
2019-04-14 15:06:06 +0800

    验证sign函数

--- shared-car-bms/src/main/java/top/gamewan/bms/sharedcarbms/Controller/UserController.java
+++ shared-car-bms/src/main/java/top/gamewan/bms/sharedcarbms/Controller/UserController.java
@@ -2,31 +2,29 @@ package top.gamewan.bms.sharedcarbms.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import top.gamewan.bms.sharedcarbms.Bean.LoginReturnMsg;
 import top.gamewan.bms.sharedcarbms.Bean.ReturnMsg;
-import top.gamewan.bms.sharedcarbms.Bean.UserInfo;
+import top.gamewan.bms.sharedcarbms.utils.VerificationUtil;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Enumeration;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api")
 public class UserController {
     @Autowired
     JdbcTemplate jdbcTemplate;
-
+    @Autowired
+    VerificationUtil verificationUtil;
     @RequestMapping(method = RequestMethod.POST,value = "login")
-
     public ReturnMsg login(HttpServletRequest request){
-        String username=request.getParameter("username");
-        if(username!=null && !username.equals(""))
-        return new LoginReturnMsg("登录成功",1000,null);
-        else
-            return new LoginReturnMsg("登录失败",1001,null);
+       Map<String,String> map=VerificationUtil.requestToMap(request);
+       if(!verificationUtil.checkSign(map)){
+           return new LoginReturnMsg("sign验证失败",1001,null);
+       }
+       return new LoginReturnMsg("sign验证成功",1000,null);
     }
 }
---
+++ shared-car-bms/src/main/java/top/gamewan/bms/sharedcarbms/utils/VerificationUtil.java
@@ -0,0 +1,52 @@
+package top.gamewan.bms.sharedcarbms.utils;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+@Component
+public class VerificationUtil {
+    @Value(value="${top.gamewan.bms.signstring}")
+    private  String signString;
+    @Value("${top.gamewan.bms.timeout}")
+    private int timeout;
+    public boolean checkSign(Map<String,String> map){
+        if(map.get("timestamp")==null || map.get("timestamp").equals(""))
+            return false;
+        if(map.get("sign")==null || map.get("sign").equals(""))
+            return false;
+        Long currenttime=System.currentTimeMillis();
+        Long clienttime=Long.parseLong(map.get("timestamp"));
+        if(currenttime-clienttime>timeout){
+            return false;
+        }
+        StringBuilder signResult=new StringBuilder();
+        List<String> key=new ArrayList<>();
+        for(String cur:map.keySet()){
+            if(!cur.equals("sign"))
+            key.add(cur);
+        }
+        Collections.sort(key);
+        for(String cur:key){
+            signResult.append(cur).append(map.get(cur));
+        }
+        signResult.append(signString);
+        System.out.println(signResult.toString());
+        if(DigestUtils.md5DigestAsHex(signResult.toString().getBytes()).equals(map.get("sign"))){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static Map<String,String> requestToMap(HttpServletRequest request){
+        Enumeration<String> parametername=request.getParameterNames();
+        Map<String,String> map=new HashMap<>();
+        while (parametername.hasMoreElements()){
+            String key=parametername.nextElement();
+            map.put(key,request.getParameter(key));
+        }
+        return map;
+    }
+}
