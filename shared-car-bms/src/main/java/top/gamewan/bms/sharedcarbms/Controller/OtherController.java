fb36b9301abf0d9745fe2c4d70073d8d8e5996b5
xuxin3101 <xuxin3101@gmail.com>
2019-05-04 22:40:17 +0800

    添加图表数组

--- shared-car-bms/src/main/java/top/gamewan/bms/sharedcarbms/Controller/OtherController.java
+++ shared-car-bms/src/main/java/top/gamewan/bms/sharedcarbms/Controller/OtherController.java
@@ -10,6 +10,8 @@ import top.gamewan.bms.sharedcarbms.Services.IndexInfoServices;
 import top.gamewan.bms.sharedcarbms.Services.UserServices;
 import top.gamewan.bms.sharedcarbms.utils.VerificationUtil;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -26,13 +28,29 @@ public class OtherController {
     public IndexInfoReturnMsg getIndexInfo(HttpServletRequest request){
         Map<String,String> map=VerificationUtil.requestToMap(request);
         if(!verificationUtil.checkSign(map)){
-            return new IndexInfoReturnMsg("sign验证失败",1001,null);
+            return new IndexInfoReturnMsg("sign验证失败",1001,null,null,null);
         }
         String token=request.getParameter("token");
         String username=request.getParameter("username");
         if(!userServices.userTokenCheck(username,token)){
-            return new IndexInfoReturnMsg("token验证失败",1002,null);
+            return new IndexInfoReturnMsg("token验证失败",1002,null,null,null);
         }
-        return new IndexInfoReturnMsg("获取成功",1000,indexInfoServices.getIndexInfo());
+        List<Integer> excepted=new ArrayList<>();
+        List<Integer> actual=new ArrayList<>();
+        excepted.add(820);
+        excepted.add(932);
+        excepted.add(901);
+        excepted.add(934);
+        excepted.add(1690);
+        excepted.add(830);
+        excepted.add(1120);
+        actual.add(620);
+        actual.add(532);
+        actual.add(901);
+        actual.add(1290);
+        actual.add(1330);
+        actual.add(1320);
+        actual.add(820);
+        return new IndexInfoReturnMsg("获取成功",1000,indexInfoServices.getIndexInfo(),excepted,actual);
     }
 }
